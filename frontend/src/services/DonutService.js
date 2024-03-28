eed5d7fee4967370f9ee905ffec3a976c3854144
YumanGong <gongymsilvia@gmail.com>
2024-03-28 17:27:08 -0500

    update adddonutsmodal

--- frontend/src/services/DonutService.js
+++ frontend/src/services/DonutService.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const REST_API_BASE_URL = 'http://localhost:8080/api/donuts/';
 
 
-// Define a function to fetch the list of donuts from the API
+// use the Axios library to make an HTTP GET request to fetch the list of donuts from the specified REST API endpoint.
 export const listDonuts = () => axios.get(REST_API_BASE_URL);
