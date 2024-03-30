c76397353af57417cbd63a12d1f7206cdf1f367b
YumanGong <gongymsilvia@gmail.com>
2024-03-30 16:06:07 -0500

    backup from main and add post request corresponding code

--- frontend/src/services/DonutService.js
+++ frontend/src/services/DonutService.js
@@ -1,9 +1,14 @@
+// Import the Axios library for making HTTP requests
 import axios from "axios";
 
+// Define the base URL of the REST API endpoint for donuts
 const REST_API_BASE_URL = 'http://localhost:8080/api/donuts/';
 
-
-// use the Axios library to make an HTTP GET request to fetch the list of donuts from the specified REST API endpoint.
+// Function to fetch the list of donuts from the REST API
 export const listDonuts = () => axios.get(REST_API_BASE_URL);
 
+// Function to add a new donut to the REST API
+export const createDonuts = (donut) => axios.post(REST_API_BASE_URL, donut);
+
+// Function to fetch a single donut from the REST API based on its ID
 export const getSingleDonut = (donutId) => axios.get(REST_API_BASE_URL + donutId);
