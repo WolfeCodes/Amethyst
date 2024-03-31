be0486365aa4a7fff48f4488ed42262bcce71ebd
YumanGong <gongymsilvia@gmail.com>
2024-03-31 00:00:22 -0500

    update header

--- frontend/src/components/frontend/Header.jsx
+++ frontend/src/components/frontend/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/frontend/Header.css'
 
@@ -6,7 +6,7 @@ const Header = () => {
 
     return (
         <div className='header'>
-            <nav className='navbar navbar-expand bg-body-tertiary'>
+            <nav className='navbar navbar-expand'>
                 <div className='container-fluid'>
                     <a className='navbar-brand' href='/'>
                         <img src="https://donutbank.com/cdn/shop/products/WhiteIcingCakewithSprinkles.png?v=1695916793" alt="Logo" width="50" height="40"></img>
@@ -21,7 +21,7 @@ const Header = () => {
                             <a className="nav-link active" aria-current="page" href="#">My Order</a>
                             <a className="nav-link" href="/cart">Cart</a>
                             <Link to="/backstage" className="nav-link btn btn-outline-primary">BackStage</Link>
-                            <button type="button" className="btn btn-success">Sign up</button>
+                            <button type="button" className="signup-bt">Sign up</button>
                         </div>
                     </div>
                 </div>
