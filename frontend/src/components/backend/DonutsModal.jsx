9984883b7a2a37e3e01d40825ac112cfdc196e1f
YumanGong <gongymsilvia@gmail.com>
2024-03-31 22:56:18 -0500

    add updating donuts

--- frontend/src/components/backend/DonutsModal.jsx
+++ frontend/src/components/backend/DonutsModal.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../styles/backend/Modal.css'
-import { createDonuts } from '../../services/DonutService';
-import { useNavigate } from 'react-router-dom';
+import { createDonuts, getSingleDonut, updateDonut } from '../../services/DonutService';
+import { useNavigate, useParams } from 'react-router-dom';
 
 
-const AddDonutsModal = ({ closeModal }) => {
+const DonutsModal = ({ closeModal, id }) => {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [imageUrl, setImageUrl] = useState('');
@@ -12,25 +12,59 @@ const AddDonutsModal = ({ closeModal }) => {
 
   const navigator = useNavigate();
 
-  function saveDonut(e) {
-    e.preventDefault();
+  useEffect(() => {
+    if (id != null) {
+      getSingleDonut(id).then((response) => {
+        console.log(response.data);
+        setName(response.data.name);
+        setPrice(response.data.price);
+        setImageUrl(response.data.imageUrl);
+        setDescription(response.data.description);
+      }).catch(error => {
+        console.error(error);
+      })
+    }
+  }, [id])
 
+  function saveOrUpdateDonut(e) {
+    e.preventDefault();
     const donut = { name, price, imageUrl, description, rating: 5 };
     console.log(donut);
-
-    createDonuts(donut).then((response) => {
-      console.log(response.data);
-      closeModal(false); // Close the modal after creating the donuts
-      navigator('/backstage/backdonuts');
-    })
+    if (id != null) {
+      updateDonut(id, donut).then((response) => {
+        console.log(response.data);
+        closeModal(false); // Close the modal after creating the donuts
+        navigator('/backstage/backdonuts');
+      }).catch(error => {
+        console.error(error);
+      })
+    } else {
+      createDonuts(donut).then((response) => {
+        console.log(response.data);
+        closeModal(false); // Close the modal after creating the donuts
+        navigator('/backstage/backdonuts');
+      }).catch(error => {
+        console.error(error);
+      })
+    }
   }
+
+  function pageTitle() {
+    if (id != null) {
+      return <h1 className="modal-title fs-5" >Update Donut</h1>
+
+    } else {
+      return <h1 className="modal-title fs-5" >Add Donut</h1>
+    }
+  }
+
 
   return (
     <div className="modal-container">
       <div className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <h1 className="modal-title fs-5" >Add Donuts</h1>
+            {pageTitle()}
             <button
               type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => closeModal(false)}  >
             </button>
@@ -86,7 +120,7 @@ const AddDonutsModal = ({ closeModal }) => {
             <button type="button" className="btn btn-secondary" onClick={() => closeModal(false)}>
               Close
             </button>
-            <button type="button" className="btn btn-primary" onClick={saveDonut}>
+            <button type="button" className="btn btn-primary" onClick={saveOrUpdateDonut}>
               Submit
             </button>
           </div>
@@ -96,4 +130,4 @@ const AddDonutsModal = ({ closeModal }) => {
   );
 }
 
-export default AddDonutsModal
+export default DonutsModal
--- frontend/src/services/DonutService.js
+++ frontend/src/services/DonutService.js
@@ -13,5 +13,8 @@ export const createDonuts = (donut) => axios.post(REST_API_BASE_URL, donut);
 // Function to fetch a single donut from the REST API based on its ID
 export const getSingleDonut = (donutId) => axios.get(REST_API_BASE_URL + donutId);
 
+// Function to update a single donut from the Database based on its ID
+export const updateDonut = (donutId, donut) => axios.put(REST_API_BASE_URL + donutId, donut);
+
 // Function to delete a single donut from the Database based on its ID
 export const deleteDonutById = (donutId) => axios.delete(REST_API_BASE_URL + donutId);
