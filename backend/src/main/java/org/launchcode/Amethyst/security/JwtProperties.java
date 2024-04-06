b01447b9e1c5964ec660fbcc1b3ffb0801201cc6
kalenduhs <kalenduhs@gmail.com>
2024-04-06 00:23:02 -0500

    secretKey config

---
+++ backend/src/main/java/org/launchcode/Amethyst/security/JwtProperties.java
@@ -0,0 +1,12 @@
+package org.launchcode.Amethyst.security;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("security.jwt")
+public class JwtProperties {
+
+    private String secretKey;
+
+}
