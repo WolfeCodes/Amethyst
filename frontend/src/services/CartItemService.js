f0f8324a8a9763e90d77a403e0539c9382378b3f
YumanGong <gongymsilvia@gmail.com>
2024-04-17 20:57:07 -0500

    added the cart item deletion

--- frontend/src/services/CartItemService.js
+++ frontend/src/services/CartItemService.js
@@ -4,4 +4,6 @@ const REST_API_BASE_URL = 'http://localhost:8080/api/cartItem/';
 
 export const getCartItemById = (cartItemId) => axios.get(REST_API_BASE_URL + cartItemId);
 
+export const deleteCartItemById = (cartItemId) => axios.delete(REST_API_BASE_URL + cartItemId);
+
 export const updateCartItemQuantity = (cartItemId, quantity) => axios.put(REST_API_BASE_URL + cartItemId + '/' + 'quantity/' + quantity);
